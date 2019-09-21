293b7121e1418445b955307389dce1987ecd15e5
darkost12 <50777238+darkost12@users.noreply.github.com>
2019-09-21 10:18:58 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,158 +1,151 @@
-const token = "AgAAAAA0yS9YAAW1uDyTB0tMu09ulCsTQ-WxhDw";
 /**
- *Gets songs from yadi through REST API and puts in the custom WEB-player. 
+ * Gets songs from yadi through REST API and puts in the custom WEB-player. 
  */
-	var received_links = [];	//The information received from the API-call --
-	var received_names = [];		//-- before shuffle.
-	var transitional_links=[];	//Are used only during --
-	var transitional_names=[];		// -- the shuffle process.
-	var shuffled_names =[];		//The information after --
-	var shuffled_links =[];			// --  the shuffle process. Ready to be put in the actual play.
-	var current_song;			//The global identificator of currently playing song
-	var play_flag;				//The Boolean flag of playing status.
-	var audiopl = document.getElementById('music_player');		//The link to the <audio> element in your HTML code.
-	var song_name = document.getElementById('song_name');		//The link to the <p>/<h> element.
-	var tog_but = document.getElementById('toggle_button');		//The link to the <img> element.
-	var vol_but = document.getElementById('volume_button');		//The link to the <img> element.
-	var position = document.getElementById('current_position');	//The link to the <input type="range"> element.
-	var timing = document.getElementById('current_time');		//The link to the <div> element.
-	var proxy = "https://pp-serv.herokuapp.com/";
-	var audioContext;
-	var visualctx;
-	var audioSrc;
-	var analyser;
-	var canvas,
-        cwidth,
-        cheight,
-        meterWidth, //width of the meters in the spectrum
-        gap, //gap between meters
-        capHeight,
-        capStyle,
-        meterNum, //count of the meters
-        capYPositionArray;
-	window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+var received_names = [];	//The information received from the API-call before shuffle.
+var transitional_names = [];//The information in the shuffle process.
+var shuffled_names = [];	//The information after the shuffle process. Ready to be put in the actual play.
+var current_song;			//The global identificator of currently playing song
+var play_flag;				//The Boolean flag of playing status.
+var audiopl = document.getElementById('music_player');		//The link to the <audio> element in your HTML code.
+var song_name = document.getElementById('song_name');		//The link to the <p>/<h> element.
+var tog_but = document.getElementById('toggle_button');		//The link to the <img> element.
+var vol_but = document.getElementById('volume_button');		//The link to the <img> element.
+var position = document.getElementById('current_position');	//The link to the <input type="range"> element.
+var timing = document.getElementById('current_time');		//The link to the <div> element.
+var audioContext, visualctx, audioSrc, analyser;			//Variables for audioContext analysis.
+var canvas, cwidth, cheight, meterWidth, gap, capHeight, capStyle, meterNum;		//Canvas and bars variables.
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;	//Automatic detection of webkit.
 
 /**
- *Shuffles music after getting through API call.
+ * Shuffles music after getting through API call.
  */
-function shuffle_music(){
+function shuffle_music() {
+	song_name.style.display = 'none';
+	audiopl.currentTime = 0;
+	play_flag = false;
 	audiopl.pause();
-	audiopl.currentTime=0;
-	song_name.style.display='none';
-	tog_but.src="res/play.png";
-	let ctr = received_links.length;	//Fisher-Yates shuffle algorithm
-	let index, temp1, temp2;
-	transitional_links=received_links;
-	transitional_names=received_names;
-	while (ctr>0){
+	tog_but.src = "res/play.png";
+	let ctr = received_names.length;	//Fisher-Yates shuffle algorithm
+	let index, temp;
+	transitional_names = received_names;
+	while (ctr>0) {
 		index = Math.floor(Math.random() * ctr);
 		ctr--;
-		temp1 = transitional_links[ctr];
-		transitional_links[ctr]=transitional_links[index];
-		transitional_links[index]=temp1;
-		temp2 = transitional_names[ctr];
-		transitional_names[ctr]=transitional_names[index];
-		transitional_names[index]=temp2;	
+		temp = transitional_names[ctr];
+		transitional_names[ctr] = transitional_names[index];
+		transitional_names[index] = temp;	
 	}
-	shuffled_links=transitional_links;
-	shuffled_names=transitional_names;
+	shuffled_names = transitional_names;
 	console.log('Shuffle was performed successfully!');
-	current_song=0;
+	current_song = 0;
 	show();
 }
 
-
 /**
- *Loads the first element of shuffled song array to the HTML. Also turns off the overlay.
+ * Loads the first element of shuffled song array to the HTML. Also turns off the overlay.
  */	
-function show(){
-		position.value=1;
-		let button = tog_but;
-		if (audiopl.paused && audiopl.src==''){
-			audiopl.src=shuffled_links[0];
-			song_name.innerHTML=shuffled_names[current_song];
-			song_name.style.display='inline-block';
-			document.getElementById('overlay').style.display="none";
-			document.getElementById('load_spinner').style.display="none";
-		}
-		if (!audiopl.paused){
-		}
-}	
+function show() {
+	position.value = 1;
+	if (audiopl.paused && audiopl.src == '') {
+		audiopl.src = get_link(shuffled_names[0]);
+		change_title();
+		document.getElementById('overlay').style.display = "none";
+		document.getElementById('load_spinner').style.display = "none";
+	}
+}
 
 /**
- *Sets the logic of toggle button.
+ * Changes title of song on switch. Also removes extension from the title.
+ */	
+function change_title() {
+	let title = shuffled_names[current_song];
+	song_name.innerHTML = title.slice(0, (title.length - 4));
+	if (song_name.style.display == 'none')
+		song_name.style.display = 'inline-block';
+}
+
+/**
+ * Actually toggles music and changes button source image.
  */
-function toggle_music(){
-	let button = tog_but;
-	if (audiopl.src!='' && audiopl.paused){
-		open_context();
+function toggle() {
+	if (play_flag == true) {
 		audiopl.play();
-		play_flag=true;
-		button.src="res/pause.png";
-		song_name.innerHTML=shuffled_names[current_song];
-		song_name.style.display='inline-block';
-	} else if (audiopl.src!='' && !audiopl.paused){
-		close_context();
+		tog_but.src = "res/pause.png";
+	} else {
 		audiopl.pause();
-		play_flag=false;
-		button.src="res/play.png"
-		song_name.innerHTML=shuffled_names[current_song];
-		song_name.style.display='inline-block';
-	} else if (audiopl.src==''){}
-}
-
-/**
- *Sets the logic of next song button.
- */
-function next_music(){
-	close_context();
-	tog_but.src="res/pause.png";
-	position.value=1;
-	audiopl.pause();
-	play_flag=false;
-	audiopl.currentTime = 0;
-	current_song+=1;
-	if (current_song>(shuffled_links.length-1)){
-		current_song=0;
+		tog_but.src = "res/play.png"
 	}
-	audiopl.src = shuffled_links[current_song];
-	open_context();
-	audiopl.play();
-	play_flag=true;
-	song_name.innerHTML=shuffled_names[current_song];
-	song_name.style.display='inline-block';
+}
+/**
+ * Sets the logic of toggle button. Also opens/closes contexts.
+ */
+function toggle_music() {
+	if (audiopl.src != '' && audiopl.paused)
+		open_context();
+	else if (audiopl.src != '' && !audiopl.paused)
+		close_context();
+	toggle();
 }
 
 
 /**
- *Sets the logic of previous song button.
+ * Sets the logic of next song button. Also changes visuals.
  */
-function previous_music(){
-	tog_but.src="res/pause.png";
-	position.value=1;
+function next_music() {
 	close_context();
-	audiopl.pause();
-	play_flag=false;
-	audiopl.currentTime = 0;
-	current_song-=1;
-	if (current_song<0)
-	{
-		current_song=shuffled_links.length-1;
-	}
-	audiopl.src = shuffled_links[current_song];
+	reset_defaults();
+	current_song += 1;
+	if (current_song > (shuffled_names.length - 1))
+		current_song = 0;
+	audiopl.src = get_link(shuffled_names[current_song]);
 	open_context();
-	audiopl.play();
-	play_flag=true;
-	song_name.innerHTML=shuffled_names[current_song];
-	song_name.style.display='inline-block';
+	toggle();
+	change_title();
 }
-function open_context(){
+
+/**
+ * Resets slider variables to defaults.
+ */
+function reset_defaults() {
+	position.value = 1;
+	audiopl.currentTime = 0;
+}
+
+/**
+ * Sets the logic of previous song button. Also visual changes.
+ */
+function previous_music() {
+	close_context();
+	reset_defaults();
+	current_song -= 1;
+	if (current_song < 0)
+		current_song = shuffled_names.length - 1;
+	audiopl.src = get_link(shuffled_names[current_song]);
+	open_context();
+	toggle();
+	change_title();
+}
+
+/**
+ * Gets signed link via GET request using "Key" parameter.
+ * @param {string} title. Song[i].Key (title of song).
+ * @return {string} url. Signed url for audio.src.
+ */
+function get_link(title) {
+	return "https://public-music-storage.s3.amazonaws.com/" + title;
+}
+
+/**
+ * Opens new context for current song.
+ */
+function open_context() {
+	play_flag = true;
+	// If there is no active audio context, it opens new.
 	if(audioContext === undefined) {
 		audioContext = new AudioContext();
-		if (audioSrc === undefined){
+		// If the source is already connected to context, nothing happens.
+		if (audioSrc === undefined)
 			audioSrc = audioContext.createMediaElementSource(audiopl);
-		}
-		
 		console.log("Created AudioContext");
 		console.log("Sample rate: " + audioContext.sampleRate);
 	}
@@ -164,20 +157,17 @@ function open_context(){
 	
 	console.log('AudioContext is up, sample rate: ' + audioContext.sampleRate);
 	
+	/**
+	 * Gets pixel ratio of current device for correct drawing on canvas with high sharpness.
+ 	 * @param {canvas object} canvas.
+	 * @return {canvas_context} ctx.
+	 */
 	function setupCanvas(canvas) {
-		// See https://www.html5rocks.com/en/tutorials/canvas/hidpi/
-		
-		// Get the device pixel ratio, falling back to 1.
 		var dpr = window.devicePixelRatio || 1;
-		// Get the size of the canvas in CSS pixels.
 		var rect = canvas.getBoundingClientRect();
-		// Give the canvas pixel dimensions of their CSS
-		// size * the device pixel ratio.
 		canvas.width = rect.width * dpr;
 		canvas.height = rect.height * dpr;
 		var ctx = canvas.getContext('2d');
-		// Scale all drawing operations by the dpr, so you
-		// don't have to worry about the difference.
 		ctx.scale(dpr, dpr);
 		return ctx;
 	}
@@ -186,30 +176,36 @@ function open_context(){
 	visualctx = setupCanvas(canvas);
 	
 	
-    renderFrame();
+    render_frame();
 }
 
-async function close_context(){
-	audioSrc.disconnect();
-	analyser.disconnect();
-	console.log('AudioContext has been closed');
+/**
+ * Stops music, resets slider variables to minimum, and closes context on switching to the next song.
+ */
+async function close_context() {
+	play_flag = false;
+	audiopl.pause();
+	if(audioContext !== undefined) {
+		audioSrc.disconnect();
+		analyser.disconnect();
+		console.log('AudioContext has been closed');
+	}
 }
 
-function renderFrame() {
+/**
+ * Draws new frame of spectrum visualization.
+ */
+function render_frame() {
     const ctx = visualctx;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     const capHeight = 2;
-    
     const barSpacing = 15;
     const barWidth = 7;
     const barHeight = canvas.height - capHeight;
-    
     const nOfBars = Math.round(canvas.width/barSpacing);
     
     const frequencyData = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(frequencyData);
-    // analyser.getByteTimeDomainData(frequencyData);
     
     const styles = {
     	cap_style: '#fff',
@@ -221,33 +217,32 @@ function renderFrame() {
 		    return g;
     	})()
     };
-    
+    // Setting of upper limit on bars so there is no empty bars at the end any more.
     const frequencyUpper = audioContext.sampleRate / 2;
     const frequencyLimit = Math.min(16e3, frequencyUpper);
     
+    // Frequency step per bar.
     const step = 
     	(frequencyData.length * (frequencyLimit / frequencyUpper) - 1) 
 		/ (nOfBars - 1);
     
     for(let i = 0; i < nOfBars; i++) {
-    	// Normalized value (0..1)
+    	// Normalized value (0..1) - proportion 'bar_height/canvas_height'.
     	const value = frequencyData[Math.floor(i * step)] / 255;
-    	
+    	//  _____________
     	// |------- y = 0
     	// |---||-- y = capHeight
     	// |---||--
     	// |---||--
     	// |---||-- y = barHeight + capHeight
-    	
+    
     	// Bar
     	ctx.fillStyle = styles.gradient;
-    	ctx.fillRect(
-    		barSpacing / 2 + i * barSpacing,
+    	ctx.fillRect(barSpacing / 2 + i * barSpacing,
     		barHeight - barHeight * value + capHeight,
     		barWidth,
     		barHeight - barHeight * (1 - value)
     	);
-    	
      	// Top of the bar (cap)
     	ctx.fillStyle = styles.cap_style;
     	ctx.fillRect(
@@ -257,119 +252,111 @@ function renderFrame() {
     		capHeight
     	);
     }
-   
-    requestAnimationFrame(renderFrame);
+   	// Closure
+    requestAnimationFrame(render_frame);
 }
 
 /**
- *The boot and the listeners' logic.
+ * The boot and the listeners' logic.
  */
-window.onload=function(){
+window.onload = function() {
 	/**
-	 *Loads the overlay at the very beginning.
+	 * Loads the overlay at the very beginning.
 	 */
-	document.getElementById('overlay').style.display="block";
-	document.getElementById('load_spinner').style.display="block";
-
-		/**
-		 *REST API call. Returns direct links to songs and their names.
-		 */
-	$.ajax({
-		url: 'https://cloud-api.yandex.net:443/v1/disk/resources/files?limit=2000',
-            type: 'GET',
-            dataType: 'json',
-            contentType: "application/json",
-            beforeSend: function(xhr) {
-                 xhr.setRequestHeader("Authorization", token)
-            },
-            success: function(data){
-            	for (let i=0; i<(data.items).length;i++){
-            		received_names.push(((data.items)[i].name).slice(0,((data.items)[i].name).length-4));
-            		received_links.push((data.items)[i].file);
-            	}
-            	console.log('Songs were received successfully!');
-            	shuffle_music();
-            }
-        });
+	(function init_overlay() {
+		document.getElementById('overlay').style.display = "block";
+		document.getElementById('load_spinner').style.display = "block";
+	})();
 	
-		/**
-		 *Switches to the next song if the previous has ended.
-		 */
-	audiopl.addEventListener("ended", ended_next);
-	function ended_next(){
-		current_song+=1;
-			if (current_song>(shuffled_links.length-1)){
-				current_song=0;
+	/**
+	 * Ajax call. Gets information in "Key" field of objects on disk.
+	 */
+	(function load_songs() {
+		$.ajax({
+			type: "GET",
+			url: 'https://public-music-storage.s3.amazonaws.com/',
+			dataType: "xml",
+			success: function (xml) {
+				$(xml).find('Key').each(function() {
+					received_names.push(this.innerHTML);
+				});
+	            console.log("Songs were received successfully!");
+	            shuffle_music();
 			}
-		song_name.innerHTML=shuffled_names[current_song];
-		audiopl.src=shuffled_links[current_song];
+		});
+	})();
+	
+	/**
+	 * Switches to the next song if the previous has ended.
+	 */
+	audiopl.addEventListener("ended", ended_next);
+
+	function ended_next() {
+		current_song += 1;
+		if (current_song > (shuffled_names.length - 1))
+			current_song = 0;
+		change_title();
+		audiopl.src = get_link(shuffled_names[current_song]);
 		audiopl.play();
-		tog_but.src="res/pause.png";
 	}
 
-		/**
-		 *Updates displayed current time.
-		 */
+	/**
+	 * Updates displayed current time.
+	 */
 	position.addEventListener('input', change_time);
-	function change_time(){
-		if (audiopl.paused){
-			play_flag=false;
-		}
-		else {
-			play_flag=true;
-		}
-		audiopl.currentTime=audiopl.duration/100*position.value;
-		if (Math.floor(audiopl.currentTime%60)<10){
-			timing.innerHTML=Math.floor(audiopl.currentTime/60)+':0'+Math.floor(audiopl.currentTime%60);
-		} else {
-			timing.innerHTML=Math.floor(audiopl.currentTime/60)+':'+Math.floor(audiopl.currentTime%60);
-		}
-		if (play_flag==true){
+
+	function change_time() {
+		if (audiopl.paused)
+			play_flag = false;
+		else
+			play_flag = true;
+		audiopl.currentTime = audiopl.duration / 100 * position.value;
+		if (Math.floor(audiopl.currentTime % 60) < 10)
+			timing.innerHTML = Math.floor(audiopl.currentTime / 60) + ':0' + Math.floor(audiopl.currentTime % 60);
+		else
+			timing.innerHTML = Math.floor(audiopl.currentTime / 60) + ':' + Math.floor(audiopl.currentTime % 60);
+		if (play_flag == true)
 			audiopl.play();
-		}
 	};
 
-		/**
-		 *Moves slider according to current time.
-		 */
+	/**
+	 * Moves slider according to current time.
+	 */
 	audiopl.addEventListener("timeupdate", move_slider); 
-	function move_slider(){
-		if (audiopl.currentTime==0){
-			position.value=1;
-		}else{
-		position.value=(audiopl.currentTime*100/audiopl.duration);
-		}
-		if (Math.floor(audiopl.currentTime%60)<10){
-			timing.innerHTML=Math.floor(audiopl.currentTime/60)+':0'+Math.floor(audiopl.currentTime%60);
-		} else {
-			timing.innerHTML=Math.floor(audiopl.currentTime/60)+':'+Math.floor(audiopl.currentTime%60);
-		}
+
+	function move_slider() {
+		if (audiopl.currentTime == 0)
+			position.value = 1;
+		else
+			position.value = (audiopl.currentTime * 100 / audiopl.duration);
+		
+		if (Math.floor(audiopl.currentTime % 60) < 10)
+			timing.innerHTML = Math.floor(audiopl.currentTime / 60) + ':0' + Math.floor(audiopl.currentTime % 60);
+		else 
+			timing.innerHTML = Math.floor(audiopl.currentTime / 60) + ':' + Math.floor(audiopl.currentTime % 60);
 	}
 
 	/**
-		 *Changes the volume according to the slider position.
-		 */
+	 * Changes the volume according to the slider position.
+	 */
 	document.getElementById('volume_regulator').addEventListener("input", change_volume);
-	function change_volume(){
+
+	function change_volume() {
 		audiopl.volume = document.getElementById('volume_regulator').value;
 	}
 
 	/**
-		 *Toggles mute on click.
-		 */
+	 * Toggles mute on click.
+	 */
 	vol_but.addEventListener("click", toggle_mute);
-	function toggle_mute(){
-		if (audiopl.src==''){
-		}
-		if (!audiopl.muted){
-			audiopl.muted=true;
-			vol_but.src="res/mute.png";
-		} else{
-			audiopl.muted=false;
-			vol_but.src="res/volume.png";
+
+	function toggle_mute() {
+		if (!audiopl.muted) {
+			audiopl.muted = true;
+			vol_but.src = "res/mute.png";
+		} else {
+			audiopl.muted = false;
+			vol_but.src = "res/volume.png";
 		}
 	}
-
 }
-
-
