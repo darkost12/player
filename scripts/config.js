58b59b7ad323d65582d9346155d576ba14fe65c8
darkost12 <k.akhanshchikov@planado.app>
2026-04-27 19:50:26 +0400

    Move to OCI

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,8 @@
   window.APP_CONFIG = {
     FORCE_PATH_STYLE: true,
     BUCKET: 'darkost-public', // Bucket name,
-    ENDPOINT: 'storage.yandexcloud.net/', // Don't include protocol
+    ENDPOINT:
+      'frzcyvp0c9ei.compat.objectstorage.eu-frankfurt-1.oraclecloud.com', // Don't include protocol
     SUBPATH: 'music/', // Path inside your S3
     METADATA: 'metadata/', // Path inside your S3 for .yml metadata files
     ACCESS_KEY: '', // Access key if bucket is private (optional)
